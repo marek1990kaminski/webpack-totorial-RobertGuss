c5abad1ee0481f1bf1b2dcee98dba0b8906f9c04
Marek K <marekkaminski26@gmail.com>
2017-11-26 16:15:39 +0100

    now im doing css bundling

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,20 @@
 const path = require('path');
 
 module.exports = {
-    entry:'./src/index.js',
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
     }
 };
