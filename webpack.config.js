81d8db30a049f6af5c4f7ffa3dd5244a145d4794
Marek K <marekkaminski26@gmail.com>
2017-11-27 13:03:06 +0100

    I've done some sourcemapping

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+    devtool: 'inline-source-map',
     module: {
         rules: [
             {
@@ -21,7 +22,11 @@ module.exports = {
                 test: /\.scss$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',
-                    use: ['css-loader', 'postcss-loader', 'sass-loader']
+                    use: [
+                        {loader: 'css-loader', options: {sourceMap: true}},
+                        {loader: 'postcss-loader', options: {sourceMap: true}},
+                        {loader: 'sass-loader', options: {sourceMap: true}}
+                    ]
                 })
             },
             {
