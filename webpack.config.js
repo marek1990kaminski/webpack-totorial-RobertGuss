d6cc373aebc459dbd9728a7a858562abaa5c7ddb
Marek K <marekkaminski26@gmail.com>
2017-11-29 19:38:19 +0100

    last littl ecommit

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     entry: {
         app: './src/index.js',
         about: './src/js/about.js'
-    }/*'./src/index.js'*/,
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
